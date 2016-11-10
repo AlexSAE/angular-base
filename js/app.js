b2bfa1f81e5cf8a5de7640f1acd078dfde0d80f2
Branko Djurkovic <brankko@gmail.com>
2016-11-10 17:34:47 +0100

    partials

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/home.html',
+		controller: 'exampleController'
+	})
+	.when('/home', {
+		redirectTo: '/'
+	})
+	.when('/contact', {
+		templateUrl: 'partials/contact.html',
+		controller: 'drugiController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+
+});
 
 myApp.controller('exampleController', function($scope) {
 
