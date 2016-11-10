84b61daa1cfe7f8bbd6838029857935cc9cae9fb
Branko Djurkovic <brankko@gmail.com>
2016-11-10 17:15:49 +0100

    model example

--- js/app.js
+++ js/app.js
@@ -4,4 +4,25 @@ myApp.controller('exampleController', function($scope) {
 
 	$scope.ime = "Ana";
 
+	$scope.users = [
+		{
+			name: 'Marko',
+			lastName: 'Markovic',
+			email: 'mmarkovic@email.net',
+			id: 51
+		},
+		{
+			name: 'Ana',
+			lastName: 'ANICA',
+			email: 'aana@email.net',
+			id: 24
+		},
+		{
+			name: 'petar',
+			lastName: 'petroviC',
+			email: 'ppera@email.net',
+			id: 14
+		}
+	];
+
 });
